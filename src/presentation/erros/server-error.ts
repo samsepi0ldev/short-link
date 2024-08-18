a41d0c521e30b9b8bee443a330a6ca116aab7852
Elivelton Santos <samsepi0ldev@gmail.com>
2024-08-17 23:35:50 -0300

    refactor: fix name folder erros to errors

--- src/presentation/erros/server-error.ts
+++
@@ -1,7 +0,0 @@
-export class ServerError extends Error {
-  constructor (error?: Error) {
-    super('Server fails, try again soon')
-    this.name = 'ServerError'
-    this.stack = error?.stack
-  }
-}
