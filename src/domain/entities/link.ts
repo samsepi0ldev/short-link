f03f239ad8ff0203a6c149b193e0706130257121
Elivelton Santos <samsepi0ldev@gmail.com>
2024-08-19 00:23:06 -0300

    chore: create link entity and add return output interface

--- src/domain/entities/link.ts
+++ src/domain/entities/link.ts
@@ -2,6 +2,7 @@ export class Link {
   id!: string
   slug!: string
   url!: string
+  short_url!: string
   created_at!: Date
 
   constructor (props: Link) {
--- src/domain/usecases/create-short-link.ts
+++ src/domain/usecases/create-short-link.ts
@@ -1,3 +1,5 @@
+import { Link } from '@/domain/entities/link'
+
 export abstract class CreateShortLink {
   abstract create (input: CreateShortLink.Input): Promise<CreateShortLink.Output>
 }
@@ -7,7 +9,5 @@ export namespace CreateShortLink {
     url: string
     slug: string
   }
-  export type Output = {
-    id: string
-  }
+  export type Output = Link
 }
