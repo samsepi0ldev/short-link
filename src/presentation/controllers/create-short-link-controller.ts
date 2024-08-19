2f4246f804ca9b3c83b68c1f0a00a0dbeb3693c5
Elivelton Santos <samsepi0ldev@gmail.com>
2024-08-19 00:08:25 -0300

    refactor: fix space in filename

---
+++ src/presentation/controllers/create-short-link-controller.ts
@@ -0,0 +1,35 @@
+import { HttpResponse } from '@/presentation/protocols/http'
+import { Controller } from '@/presentation/controllers/controller'
+import { CreateShortLink } from '@/domain/usecases/create-short-link'
+import { requestHandle } from '@/utils/request-handle'
+import { badRequest, ok, serverError } from '@/presentation/http/http-helper'
+import { Validator } from '@/presentation/protocols/validator'
+import { BuilderValidation as Builder } from '@/presentation/validations/builder'
+
+type Request = {
+  url: string
+  slug: string
+}
+
+export class CreateShortLinkController extends Controller {
+  constructor (private readonly createShortLink: CreateShortLink) {
+    super()
+  }
+
+  async perform({ url, slug }: Request): Promise<HttpResponse> {
+    const [error, result] = await requestHandle(this.createShortLink.create({ url, slug }))
+
+    if (error !== null) {
+      return serverError(error)
+    }
+
+    return ok(result)
+  }
+
+  override buildValidator({ url, slug }: Request): Validator[] {
+    return [
+      ...Builder.of({ value: url, field: 'url' }).required().build(),
+      ...Builder.of({ value: slug, field: 'slug' }).required().build()
+    ]
+  }
+}
