8a6f8f8424fde046e9344ced1c3f259be47d11c3
Elivelton Santos <samsepi0ldev@gmail.com>
2024-08-20 16:28:07 -0300

    chore: configure monorepo and create nextjs project

---
+++ packages/server/src/main/index.ts
@@ -0,0 +1,4 @@
+export * from './factories/controllers/create-short-link-controller-factory'
+export * from './factories/controllers/get-original-link-controller-factory'
+export * from './factories/usecases/create-short-link-factory'
+export * from './factories/usecases/get-original-url-factory'
