47a1f0be022053366b5e1e7d3276bc66f494aa3b
Elivelton Santos <samsepi0ldev@gmail.com>
2024-08-20 16:47:11 -0300

    chore: create monorepo and add project nextjs

--- app/web/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { makeCreateShortLink } from '@short-link/server'
-
-export default function Home() {
-  return (
-    <h1>Hello World</h1>
-  )
-}
---
+++ packages/server/index.ts
@@ -0,0 +1,4 @@
+export * from './src/main/factories/controllers/create-short-link-controller-factory'
+export * from './src/main/factories/controllers/get-original-link-controller-factory'
+export * from './src/main/factories/usecases/create-short-link-factory'
+export * from './src/main/factories/usecases/get-original-url-factory'
